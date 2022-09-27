a363bcf70666d5d20d18bcff145c43a1625f14fb
Brandon Franks <bgfranks.dev@gmail.com>
2022-09-27 19:31:53 -0400

    pulls user data to the home page after OTP sign in

--- server/createContext.ts
+++ server/createContext.ts
@@ -1,5 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { verifyJwt } from '../ultils/jwt'
 import { prisma } from '../ultils/prisma'
+
+interface CtxUser {
+  id: string
+  email: string
+  name: string
+  iat: string
+  exp: number
+}
+
+function getUserFromReq(req: NextApiRequest) {
+  const token = req.cookies.token
+
+  if (token) {
+    try {
+      const verified = verifyJwt<CtxUser>(token)
+      return verified
+    } catch (e) {
+      return null
+    }
+  }
+
+  return null
+}
 
 export function createContext({
   req,
@@ -8,10 +32,13 @@ export function createContext({
   req: NextApiRequest
   res: NextApiResponse
 }) {
+  const user = getUserFromReq(req)
+
   return {
     res,
     req,
     prisma,
+    user,
   }
 }
 
