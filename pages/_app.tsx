348a00a3744f5c11482c2bf46d3a5452593a78f8
Brandon Franks <bgfranks.dev@gmail.com>
2022-09-28 15:13:11 -0400

    created the user context to redirect to create post page if logged in, and redirects to login if no user is signed in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,28 @@ import { withTRPC } from '@trpc/next'
 import { loggerLink } from '@trpc/client/links/loggerLink'
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
 import superjson from 'superjson'
-import { appRouter } from '../server/route/app.router'
+import { AppRouter } from '../server/route/app.router'
 import { url } from '../constants'
+import { trpc } from '../ultils/trpc'
 
 import '../styles/globals.css'
+import { UserContextProvider } from '../context/user.context'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { data, error, isLoading } = trpc.useQuery(['users.me'])
+
+  if (isLoading) {
+    return <>Loading User...</>
+  }
+
+  return (
+    <UserContextProvider value={data}>
+      <Component {...pageProps} />
+    </UserContextProvider>
+  )
 }
 
-export default withTRPC<appRouter>({
+export default withTRPC<AppRouter>({
   config({ ctx }) {
     const links = [
       loggerLink(),
--- server/route/app.router.ts
+++ server/route/app.router.ts
@@ -1,6 +1,6 @@
 import { createRouter } from '../createRouter'
 import { userRouter } from './user.router'
 
-export const appRouter = createRouter().merge('users.', userRouter)
+export const AppRouter = createRouter().merge('users.', userRouter)
 
-export type appRouter = typeof appRouter
+export type AppRouter = typeof AppRouter
